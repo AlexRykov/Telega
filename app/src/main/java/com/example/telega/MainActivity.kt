e513c47b1baa72560dca34ab0426c587c7029b9a
AlexRykov1 <rykov818@gmail.com>
2023-03-18 17:34:08 +0300

    lesson 3 commit

--- app/src/main/java/com/example/telega/MainActivity.kt
+++ app/src/main/java/com/example/telega/MainActivity.kt
@@ -2,19 +2,23 @@ package com.example.telega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import android.widget.Toolbar
 import com.example.telega.databinding.ActivityMainBinding
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mDrawer:Drawer
-    private lateinit var mHeader:AccountHeader
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
     private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         createDrawer()
     }
 
+//      Create left side thing with options
     private fun createDrawer() {
         mDrawer = DrawerBuilder()
             .withActivity(this)
@@ -46,10 +51,63 @@ class MainActivity : AppCompatActivity() {
                     .withIconTintingEnabled(true)
                     .withName("Create Group")
                     .withSelectable(false)
+                    .withIcon(R.drawable.people_outline),
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Start SecretChat")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.lock),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Create Channel")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.flight_takeoff),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Contacts")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.person_outline),
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName("Calls")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.settings_phone),
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName("Favourites")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.bookmark),
+                PrimaryDrawerItem().withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName("Settings")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.settings),
+                DividerDrawerItem(),
+                PrimaryDrawerItem().withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName("Invite Friends")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.person_add_alt),
+                PrimaryDrawerItem().withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName("Questions About")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.contact_support)
             )
+            .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    Toast.makeText(applicationContext, "Click on $position", Toast.LENGTH_SHORT).show()
+                    return false
+                }
+            })
             .build()
     }
 
+//      Create top side thing with app name
     private fun crateHeader() {
         mHeader = AccountHeaderBuilder()
             .withActivity(this)
